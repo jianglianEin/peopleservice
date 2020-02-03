0c0188df35112627bda79c73a85ad52a299d2e8f
jianglianEin <913057041@qq.com>
2020-02-03 17:47:39 +0800

    [JiShi] add swagger for api test

--- src/main/kotlin/com/microservice/peopleservice/resource/PeopleResource.kt
+++ src/main/kotlin/com/microservice/peopleservice/resource/PeopleResource.kt
@@ -1,5 +1,6 @@
 package com.microservice.peopleservice.resource
 
+import com.microservice.peopleservice.dao.User
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -10,5 +11,10 @@ class PeopleResource() {
     fun hello(): String {
         System.out.println("run in PeopleService")
         return "hello world"
+    }
+
+    @GetMapping("/user/login")
+    fun login(): User {
+        return User("jianglianEin","","/icon/image.jpg", "913057041@qq.com", 1)
     }
 }
