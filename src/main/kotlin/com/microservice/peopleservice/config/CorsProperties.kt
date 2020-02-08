46ec9d4c3bf5c4a616ab21c6bffc2dabf6010b92
jianglianEin <913057041@qq.com>
2020-02-08 15:49:27 +0800

    [JiShi] Specified application file position.

--- src/main/kotlin/com/microservice/peopleservice/config/CorsProperties.kt
+++ src/main/kotlin/com/microservice/peopleservice/config/CorsProperties.kt
@@ -2,8 +2,10 @@ package com.microservice.peopleservice.config
 
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.PropertySource
 
 @Configuration
+@PropertySource(value = ["classpath:application*"], ignoreResourceNotFound = true)
 class CorsProperties {
     @Value("\${cors.url}")
     lateinit var url: String
