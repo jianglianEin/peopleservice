415a2a10baa30c649e8ce26b6ee5e38eb3abb79d
jianglianEin <913057041@qq.com>
2020-02-08 16:29:51 +0800

    [JiShi] Use gcloud to deploy docker image and remove useless file.

--- src/test/kotlin/com/microservice/peopleservice/PeopleserviceApplicationTests.kt
+++ src/test/kotlin/com/microservice/peopleservice/PeopleserviceApplicationTests.kt
@@ -2,10 +2,9 @@ package com.microservice.peopleservice
 
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.test.context.TestPropertySource
+
 
 @SpringBootTest
-@TestPropertySource("classpath:application-test.properties")
 class PeopleserviceApplicationTests {
 
     @Test
