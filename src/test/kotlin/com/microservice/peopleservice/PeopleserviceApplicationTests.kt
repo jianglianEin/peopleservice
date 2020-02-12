9d26e8028195d7c1d861256b7baaf11d60a2fc6f
jianglianEin <913057041@qq.com>
2020-02-12 23:44:20 +0800

    [JiShi] Mock dataSource for test.

--- src/test/kotlin/com/microservice/peopleservice/PeopleserviceApplicationTests.kt
+++ src/test/kotlin/com/microservice/peopleservice/PeopleserviceApplicationTests.kt
@@ -1,7 +1,10 @@
 package com.microservice.peopleservice
 
 import org.junit.jupiter.api.Test
+import org.mockito.Mockito
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.context.annotation.Bean
+import javax.sql.DataSource
 
 
 @SpringBootTest
@@ -11,4 +14,8 @@ class PeopleserviceApplicationTests {
     fun contextLoads() {
     }
 
+    @Bean
+    fun dataSource(): DataSource? {
+        return Mockito.mock(DataSource::class.java)
+    }
 }
