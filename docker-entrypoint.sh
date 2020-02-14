d40b9af72522be36d5080a6fe1ad59f119ce0250
jianglianEin <913057041@qq.com>
2020-02-14 17:40:28 +0800

    [JiShi] remove unused env variable.

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,11 +1,4 @@
 #!/usr/bin/env bash
 
-set -e
-
-if [ "$1" = "server" ]
-then
-    echo "Run app with app.env=${APP_ENV}"
-	./gradlew --no-daemon bootRun -Dapp.env=${APP_ENV}
-else
-	eval "$@"
-fi
+echo "Run app with app.env=${APP_ENV}"
+./gradlew --no-daemon bootRun -Dapp.env=${APP_ENV}
