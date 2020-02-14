0136a4cb0f595becd302014363e05d370e6a3851
jianglianEin <913057041@qq.com>
2020-02-14 16:32:22 +0800

    [JiShi] fixed code style.

--- src/main/kotlin/com/microservice/peopleservice/service/TeamService.kt
+++ src/main/kotlin/com/microservice/peopleservice/service/TeamService.kt
@@ -33,20 +33,21 @@ class TeamService {
         val oldTeamOptional = teamRepository.findById(updateTeam.id!!)
         if (!oldTeamOptional.isEmpty) {
             val oldTeam = oldTeamOptional.get()
+            oldTeam.description = updateTeam.description
 
-            if (!oldTeam.teamname.equals(updateTeam.teamname)){
+            if (!oldTeam.teamname.equals(updateTeam.teamname)) {
                 oldTeam.teamname = updateTeam.teamname
                 val sameCreatorAndNameTeam = teamRepository.findByCreatorAndTeamname(oldTeam.creator!!, oldTeam.teamname!!)
-                if (sameCreatorAndNameTeam != null) {
-                    return Message(false, "one creator can not create same name team")
+                return when {
+                    sameCreatorAndNameTeam != null -> {
+                        Message(false, "one creator can not create same name team")
+                    }
+                    else -> {
+                        teamRepository.save(oldTeam)
+                        Message(true, "update user success")
+                    }
                 }
             }
-            if (updateTeam.description != null) {
-                oldTeam.description = updateTeam.description
-            }
-
-            teamRepository.save(oldTeam)
-            return Message(true, "update user success")
         }
 
         return Message(false, "team id do not exit")
