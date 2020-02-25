6c95f2239897c964cb487ae439be76ae0746f865
jianglianEin <913057041@qq.com>
2020-02-25 17:28:49 +0800

    [JiShi] use command tag replace path to add variable to control env.

--- pg_dump.sql
+++ pg_dump.sql
@@ -21,24 +21,27 @@ SET default_tablespace = '';
 SET default_with_oids = false;
 
 --
--- Name: team; Type: TABLE; Schema: public; Owner: postgres
+-- Name: commit; Type: TABLE; Schema: public; Owner: postgres
 --
 
-CREATE TABLE public.team (
+CREATE TABLE public.commit (
     id integer NOT NULL,
-    creator character varying(50) NOT NULL,
-    teamname character varying(50) NOT NULL,
-    description text
+    description text NOT NULL,
+    announcer character varying(50) NOT NULL,
+    receiver character varying(50) NOT NULL,
+    update_time text NOT NULL,
+    card_id integer NOT NULL,
+    is_read boolean DEFAULT false NOT NULL
 );
 
 
-ALTER TABLE public.team OWNER TO postgres;
+ALTER TABLE public.commit OWNER TO postgres;
 
 --
--- Name: team_teamid_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+-- Name: commit_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
 --
 
-CREATE SEQUENCE public.team_teamid_seq
+CREATE SEQUENCE public.commit_id_seq
     START WITH 1
     INCREMENT BY 1
     NO MINVALUE
@@ -46,220 +49,52 @@ CREATE SEQUENCE public.team_teamid_seq
     CACHE 1;
 
 
-ALTER TABLE public.team_teamid_seq OWNER TO postgres;
+ALTER TABLE public.commit_id_seq OWNER TO postgres;
 
 --
--- Name: team_teamid_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
+-- Name: commit_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
 --
 
-ALTER SEQUENCE public.team_teamid_seq OWNED BY public.team.id;
-
-
---
--- Name: user; Type: TABLE; Schema: public; Owner: postgres
---
-
-CREATE TABLE public."user" (
-    username character varying(50) NOT NULL,
-    password character varying(50) NOT NULL,
-    icon character varying(100),
-    email character varying(100) NOT NULL,
-    power integer DEFAULT 1,
-    id integer NOT NULL
-);
-
-
-ALTER TABLE public."user" OWNER TO postgres;
-
---
--- Name: user_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
---
-
-CREATE SEQUENCE public.user_id_seq
-    START WITH 1
-    INCREMENT BY 1
-    NO MINVALUE
-    NO MAXVALUE
-    CACHE 1;
-
-
-ALTER TABLE public.user_id_seq OWNER TO postgres;
-
---
--- Name: user_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
---
-
-ALTER SEQUENCE public.user_id_seq OWNED BY public."user".id;
+ALTER SEQUENCE public.commit_id_seq OWNED BY public.commit.id;
 
 
 --
--- Name: user_team_relation; Type: TABLE; Schema: public; Owner: postgres
+-- Name: commit id; Type: DEFAULT; Schema: public; Owner: postgres
 --
 
-CREATE TABLE public.user_team_relation (
-    username character varying(50) NOT NULL,
-    team_id integer NOT NULL,
-    id integer NOT NULL
-);
-
-
-ALTER TABLE public.user_team_relation OWNER TO postgres;
-
---
--- Name: user_team_relation_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
---
-
-CREATE SEQUENCE public.user_team_relation_id_seq
-    START WITH 1
-    INCREMENT BY 1
-    NO MINVALUE
-    NO MAXVALUE
-    CACHE 1;
-
-
-ALTER TABLE public.user_team_relation_id_seq OWNER TO postgres;
-
---
--- Name: user_team_relation_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
---
-
-ALTER SEQUENCE public.user_team_relation_id_seq OWNED BY public.user_team_relation.id;
+ALTER TABLE ONLY public.commit ALTER COLUMN id SET DEFAULT nextval('public.commit_id_seq'::regclass);
 
 
 --
--- Name: team id; Type: DEFAULT; Schema: public; Owner: postgres
+-- Data for Name: commit; Type: TABLE DATA; Schema: public; Owner: postgres
 --
 
-ALTER TABLE ONLY public.team ALTER COLUMN id SET DEFAULT nextval('public.team_teamid_seq'::regclass);
-
-
---
--- Name: user id; Type: DEFAULT; Schema: public; Owner: postgres
---
-
-ALTER TABLE ONLY public."user" ALTER COLUMN id SET DEFAULT nextval('public.user_id_seq'::regclass);
-
-
---
--- Name: user_team_relation id; Type: DEFAULT; Schema: public; Owner: postgres
---
-
-ALTER TABLE ONLY public.user_team_relation ALTER COLUMN id SET DEFAULT nextval('public.user_team_relation_id_seq'::regclass);
-
-
---
--- Data for Name: team; Type: TABLE DATA; Schema: public; Owner: postgres
---
-
-COPY public.team (id, creator, teamname, description) FROM stdin;
+COPY public.commit (id, description, announcer, receiver, update_time, card_id, is_read) FROM stdin;
+2	test1	jianglianEin	jianglianZwei	1581847605897	1	t
+3	test1	jianglianEin	jianglianZwei	1581848286750	2	f
 \.
 
 
 --
--- Name: team_teamid_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
+-- Name: commit_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
 --
 
-SELECT pg_catalog.setval('public.team_teamid_seq', 8, true);
-
-
---
--- Data for Name: user; Type: TABLE DATA; Schema: public; Owner: postgres
---
-
-COPY public."user" (username, password, icon, email, power, id) FROM stdin;
-jianglianEin	Ftjs1998226@		913057041@qq.com	1	1
-\.
+SELECT pg_catalog.setval('public.commit_id_seq', 3, true);
 
 
 --
--- Name: user_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
+-- Name: commit commit_pk; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
-SELECT pg_catalog.setval('public.user_id_seq', 11, true);
-
-
---
--- Data for Name: user_team_relation; Type: TABLE DATA; Schema: public; Owner: postgres
---
-
-COPY public.user_team_relation (username, team_id, id) FROM stdin;
-\.
+ALTER TABLE ONLY public.commit
+    ADD CONSTRAINT commit_pk PRIMARY KEY (id);
 
 
 --
--- Name: user_team_relation_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
+-- Name: commit_id_uindex; Type: INDEX; Schema: public; Owner: postgres
 --
 
-SELECT pg_catalog.setval('public.user_team_relation_id_seq', 7, true);
-
-
---
--- Name: team team_pk; Type: CONSTRAINT; Schema: public; Owner: postgres
---
-
-ALTER TABLE ONLY public.team
-    ADD CONSTRAINT team_pk PRIMARY KEY (id);
-
-
---
--- Name: user user_pk; Type: CONSTRAINT; Schema: public; Owner: postgres
---
-
-ALTER TABLE ONLY public."user"
-    ADD CONSTRAINT user_pk PRIMARY KEY (username);
-
-
---
--- Name: user_team_relation user_team_relation_pk; Type: CONSTRAINT; Schema: public; Owner: postgres
---
-
-ALTER TABLE ONLY public.user_team_relation
-    ADD CONSTRAINT user_team_relation_pk PRIMARY KEY (id);
-
-
---
--- Name: user_email_uindex; Type: INDEX; Schema: public; Owner: postgres
---
-
-CREATE UNIQUE INDEX user_email_uindex ON public."user" USING btree (email);
-
-
---
--- Name: user_id_uindex; Type: INDEX; Schema: public; Owner: postgres
---
-
-CREATE UNIQUE INDEX user_id_uindex ON public."user" USING btree (id);
-
-
---
--- Name: user_team_relation_id_uindex; Type: INDEX; Schema: public; Owner: postgres
---
-
-CREATE UNIQUE INDEX user_team_relation_id_uindex ON public.user_team_relation USING btree (id);
-
-
---
--- Name: team team_user_username_fk; Type: FK CONSTRAINT; Schema: public; Owner: postgres
---
-
-ALTER TABLE ONLY public.team
-    ADD CONSTRAINT team_user_username_fk FOREIGN KEY (creator) REFERENCES public."user"(username) ON UPDATE CASCADE ON DELETE CASCADE;
-
-
---
--- Name: user_team_relation user_team_relation_team_id_fk; Type: FK CONSTRAINT; Schema: public; Owner: postgres
---
-
-ALTER TABLE ONLY public.user_team_relation
-    ADD CONSTRAINT user_team_relation_team_id_fk FOREIGN KEY (team_id) REFERENCES public.team(id) ON DELETE CASCADE;
-
-
---
--- Name: user_team_relation user_team_relation_user_username_fk; Type: FK CONSTRAINT; Schema: public; Owner: postgres
---
-
-ALTER TABLE ONLY public.user_team_relation
-    ADD CONSTRAINT user_team_relation_user_username_fk FOREIGN KEY (username) REFERENCES public."user"(username);
+CREATE UNIQUE INDEX commit_id_uindex ON public.commit USING btree (id);
 
 
 --
