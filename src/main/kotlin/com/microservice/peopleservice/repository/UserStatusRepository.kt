3e4a5f16218259fe9d4d23f0dab1fef3d67b6cc7
jianglianEin <913057041@qq.com>
2020-04-16 23:10:00 +0800

    [JiShi] add expire time for redis.

--- src/main/kotlin/com/microservice/peopleservice/repository/UserStatusRepository.kt
+++ src/main/kotlin/com/microservice/peopleservice/repository/UserStatusRepository.kt
@@ -15,6 +15,7 @@ class UserStatusRepository {
     private val statusValue = "value"
     private val statusUpdateTime = "updateTime"
     private val statusUser = "username"
+    private val expireTime = 3600 * 12
 
     fun update(statusType: UserStatusType, uid: String, timestamp: Long, username: String) {
         jedisPool.resource.use { jedis ->
@@ -22,6 +23,7 @@ class UserStatusRepository {
                 hset("$uidPrefix:$uid", "$statusField:$statusValue", statusType.toString())
                 hset("$uidPrefix:$uid", "$statusField:$statusUpdateTime", timestamp.toString())
                 hset("$uidPrefix:$uid", "$statusField:$statusUser", username)
+                expire("$uidPrefix:$uid", expireTime)
             }
         }
     }
